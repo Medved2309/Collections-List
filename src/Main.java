74f2c94ef4e554b0e0a919f7608e0614624b0901
Andrey Dmitrievich Grishilo <medved2309@yandex.ru>
2022-10-19 00:23:52 +0500

    add and get list

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,36 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        ArrayList<String> shoppingList = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Введите номер операции:");
+            System.out.println("1 - Добавить в список покупок");
+            System.out.println("2 - Показать список покупок");
+            System.out.println("3 - Удалить из списка покупок");
+            int input = Integer.parseInt(scanner.nextLine());
+
+            if (input == 1) {
+                System.out.println("Какую покупку хотите добавить? Введите название");
+                String nameProduct = scanner.nextLine();
+                shoppingList.add(nameProduct);
+                System.out.println("Итого наименований в списке продуктов: " + shoppingList.size());
+
+            } else if (input == 2) {
+                System.out.println("Список добавленных продуктов:");
+                for ( int i = 0; i < shoppingList.size(); i++){
+                    System.out.println((i+1) + ". " + shoppingList.get(i));
+                }
+
+            } else if (input == 3) {
+
+            }
 
 
-
+        }
 
 
     }
