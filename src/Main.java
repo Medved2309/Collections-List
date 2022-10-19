88265969672105847f87e093c5acd8fc5d5adafa
Andrey Dmitrievich <medved2309@gmail.com>
2022-10-19 12:27:53 +0500

    reshenie while if

--- src/Main.java
+++ src/Main.java
@@ -11,27 +11,57 @@ public class Main {
             System.out.println("1 - Добавить в список покупок");
             System.out.println("2 - Показать список покупок");
             System.out.println("3 - Удалить из списка покупок");
-            int input = Integer.parseInt(scanner.nextLine());
+            System.out.println("4 - Завершить программу и показать итоговый список");
+            int operationNum = Integer.parseInt(scanner.nextLine());
 
-            if (input == 1) {
+            if (operationNum == 1) {
                 System.out.println("Какую покупку хотите добавить? Введите название");
                 String nameProduct = scanner.nextLine();
                 shoppingList.add(nameProduct);
-                System.out.println("Итого наименований в списке продуктов: " + shoppingList.size());
+                System.out.println("Добавлен: " + nameProduct + ". " + " Итого наименований в списке продуктов: " + shoppingList.size());
 
-            } else if (input == 2) {
-                System.out.println("Список добавленных продуктов:");
-                for ( int i = 0; i < shoppingList.size(); i++){
-                    System.out.println((i+1) + ". " + shoppingList.get(i));
+            } else if (operationNum == 2) {
+                vivodShoppList(shoppingList);
+
+            } else if (operationNum == 3) {
+                vivodShoppList(shoppingList);
+                System.out.println("Какую покупку хотите удалить из списка? Ведите номер или наимнование продукции:");
+                String operationDel = scanner.nextLine();
+                if (operationDel.matches("\\d")) {
+                    int numb = Integer.parseInt(operationDel) - 1;
+                    if (numb < 0 || numb > shoppingList.size()) {
+                        System.out.println("Некоректный ввод");
+                    }
+                    String list = shoppingList.get(numb);
+                    shoppingList.remove(numb);
+                    System.out.println("Покупка: " + list + " удалена.");
+                    vivodShoppList(shoppingList);
+                } else {
+                    shoppingList.remove(operationDel);
+                    System.out.println("Покупка: " + operationDel + " удалена.");
+                    vivodShoppList(shoppingList);
                 }
-
-            } else if (input == 3) {
-
+            } else if (operationNum == 4) {
+                vivodShoppList(shoppingList);
+                break;
             }
 
 
         }
-
-
     }
+
+
+    private static void vivodShoppList(ArrayList<String> shoppingList) {
+        if (shoppingList.isEmpty()) {
+            System.out.println("Список продуктов пуст");
+        } else {
+            System.out.println("Список продуктов:");
+            for (int i = 0; i < shoppingList.size(); i++) {
+                System.out.println((i + 1) + ". " + shoppingList.get(i));
+            }
+
+        }
+    }
+
+
 }
