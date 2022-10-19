cde2a70d7f0e7067b1fc5a77d92a69981a29b95c
Andrey Dmitrievich <medved2309@gmail.com>
2022-10-19 12:50:32 +0500

    reshenie while switch

--- src/Main.java
+++ src/Main.java
@@ -12,41 +12,48 @@ public class Main {
             System.out.println("2 - Показать список покупок");
             System.out.println("3 - Удалить из списка покупок");
             System.out.println("4 - Завершить программу и показать итоговый список");
-            int operationNum = Integer.parseInt(scanner.nextLine());
+            String line = scanner.nextLine();
 
-            if (operationNum == 1) {
-                System.out.println("Какую покупку хотите добавить? Введите название");
-                String nameProduct = scanner.nextLine();
-                shoppingList.add(nameProduct);
-                System.out.println("Добавлен: " + nameProduct + ". " + " Итого наименований в списке продуктов: " + shoppingList.size());
+            switch (line) {
 
-            } else if (operationNum == 2) {
-                vivodShoppList(shoppingList);
+                case ("1"): {
+                    System.out.println("Какую покупку хотите добавить? Введите название");
+                    String nameProduct = scanner.nextLine();
+                    shoppingList.add(nameProduct);
+                    System.out.println("Добавлен: " + nameProduct + ". " + " Итого наименований в списке продуктов: " + shoppingList.size());
+                    break;
 
-            } else if (operationNum == 3) {
-                vivodShoppList(shoppingList);
-                System.out.println("Какую покупку хотите удалить из списка? Ведите номер или наимнование продукции:");
-                String operationDel = scanner.nextLine();
-                if (operationDel.matches("\\d")) {
-                    int numb = Integer.parseInt(operationDel) - 1;
-                    if (numb < 0 || numb > shoppingList.size()) {
-                        System.out.println("Некоректный ввод");
-                    }
-                    String list = shoppingList.get(numb);
-                    shoppingList.remove(numb);
-                    System.out.println("Покупка: " + list + " удалена.");
-                    vivodShoppList(shoppingList);
-                } else {
-                    shoppingList.remove(operationDel);
-                    System.out.println("Покупка: " + operationDel + " удалена.");
-                    vivodShoppList(shoppingList);
                 }
-            } else if (operationNum == 4) {
-                vivodShoppList(shoppingList);
-                break;
+                case ("2"): {
+                    vivodShoppList(shoppingList);
+                    break;
+
+                }
+                case ("3"): {
+                    vivodShoppList(shoppingList);
+                    System.out.println("Какую покупку хотите удалить из списка? Ведите номер или наимнование продукции:");
+                    String operationDel = scanner.nextLine();
+                    if (operationDel.matches("\\d")) {
+                        int numb = Integer.parseInt(operationDel) - 1;
+                        if (numb < 0 || numb > shoppingList.size()) {
+                            System.out.println("Некоректный ввод");
+                        }
+                        String list = shoppingList.get(numb);
+                        shoppingList.remove(numb);
+                        System.out.println("Покупка: " + list + " удалена.");
+                        vivodShoppList(shoppingList);
+                    } else {
+                        shoppingList.remove(operationDel);
+                        System.out.println("Покупка: " + operationDel + " удалена.");
+                        vivodShoppList(shoppingList);
+                    }
+                    break;
+                }
+                case ("4"): {
+                    vivodShoppList(shoppingList);
+                    break;
+                }
             }
-
-
         }
     }
 
